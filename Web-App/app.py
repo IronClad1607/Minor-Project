84ac2e19cce56a58b336a59dcf40a82187fe23e4
Sargam Verma <vermasargam02@gmail.com>
2020-12-16 18:52:04 +0530

    adding files

--- Web-App/app.py
+++ Web-App/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import finalCode
 
 
 app = Flask(__name__)
@@ -7,6 +8,24 @@ app = Flask(__name__)
 @app.route('/')
 def home(): 
     return "<h1>Test</h1>" 
+
+@app.route('/image', methods = ['GET'])
+def wait_for_result():
+    return render_template('index.html')
+
+@app.route('/image', methods =['POST'])
+def result():
+    
+    if request.method == 'POST':
+        f = request.files['img']
+        path = './static/{}'.format(f.filename)
+        f.save(path)
+
+    image_result = finalCode.predict(f)
+
+    return render_template('index.html', result = image_result)
+
+
 
 
 
