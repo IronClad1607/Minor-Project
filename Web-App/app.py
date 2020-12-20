a2d9ef0d657085cdd692883ea6764d1e0a79eac4
Sargam Verma <vermasargam02@gmail.com>
2020-12-20 18:38:23 +0530

    creating ngrok files

--- Web-App/app.py
+++ Web-App/app.py
@@ -1,32 +1,44 @@
 from flask import Flask, render_template, redirect, request
 import finalCode
+import pandas as pd
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home(): 
-    return "<h1>Test</h1>" 
+@app.route('/', methods = ['GET'])
+def wait_for_result():
+    return render_template('hello.html')
+
+@app.route("/all", methods = ['GET', 'POST'])
+def getAll():
+    df = pd.read_excel("test.xlsx")
+    return render_template('base.html',tables=[df.to_html()],titles = ['na', 'Table'])
+
 
 @app.route('/image', methods = ['GET'])
-def wait_for_result():
+def wait_for_image_result():
     return render_template('index.html')
 
 @app.route('/image', methods =['POST'])
 def result():
     
     if request.method == 'POST':
+   
         f = request.files['img']
-        path = './static/{}'.format(f.filename)
+        path = './static/{}'.format(f.filename) 
         f.save(path)
 
     image_result = finalCode.predict(f)
+    user_final = "Sargam"
+    df = pd.read_excel("test.xlsx")
 
-    return render_template('index.html', result = image_result)
+    selected_user = df[df["Name"] == user_final]
+    dataf = pd.DataFrame({'Name': selected_user["Name"]})
 
 
 
+    return render_template('index.html',result=image_result, tables=[dataf.to_html()])
 
 
 if __name__ == '__main__':
