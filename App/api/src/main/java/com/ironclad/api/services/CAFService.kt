14582d40db709738cfbc021638cfcff1265fd636
Ishaan Jain <ishaan160799@gmail.com>
2020-12-22 01:15:12 +0530

    path one added

--- App/api/src/main/java/com/ironclad/api/services/CAFService.kt
+++ App/api/src/main/java/com/ironclad/api/services/CAFService.kt
@@ -4,9 +4,15 @@ import com.ironclad.api.models.entities.User
 import com.ironclad.api.models.response.AllUserResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CAFService {
 
     @GET("/allUser")
     suspend fun getAllUsers(): Response<List<User>>
+
+    @GET("/allUser/{id}")
+    suspend fun getUserById(
+        @Path("id") id: Int
+    ): Response<User>
 }
--- App/app/src/main/java/com/ironclad/commonidentityfinder/data/CafRepo.kt
+++ App/app/src/main/java/com/ironclad/commonidentityfinder/data/CafRepo.kt
@@ -6,4 +6,6 @@ object CafRepo {
     private val api = CAFClient.cafApi
 
     suspend fun getAllUsers() = api.getAllUsers().body()
+
+    suspend fun getUserById(id: Int) = api.getUserById(id).body()
 }
