21ab70df2344eb970469e21ceb6b6cb44cf59a85
Satvik <satvikvelidandi@gmail.com>
2020-12-21 20:28:52 +0530

    Jsonified data

--- Web-App/app.py
+++ Web-App/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, redirect, request
-# from flask_ngrok import run_with_ngrok
-import excel2json
-from flask import jsonify 
+from flask import Flask, render_template, redirect, request, jsonify
+from flask_ngrok import run_with_ngrok
 import finalCode
 import pandas as pd
 import pandas
+import json
 
 app = Flask(__name__)
 run_with_ngrok(app)
 
+df = pd.read_excel("test.xlsx")
+#a = excel2json.convert_from_file('test.xlsx')
+a = df.to_json(orient='records')
 
 @app.route('/', methods = ['GET'])
 def wait_for_result():
@@ -22,11 +24,22 @@ def getAll():
 @app.route("/allUser",methods= ['GET','POST'])
 def getAllUser():
     df = pd.read_excel("test.xlsx")
-    a = excel2json.convert_from_file('test.xlsx')
+#a = excel2json.convert_from_file('test.xlsx')
     a = df.to_json(orient='records')
-    return a
-    
+    jd = json.loads(a)
+    #print(type(a))
+    return jsonify(jd)
 
+@app.route('/allUser/<Id>')
+def getUser(Id):
+    df = pd.read_excel("test.xlsx")
+#a = excel2json.convert_from_file('test.xlsx')
+    a = df.to_json(orient='records')
+    jdata = json.loads(a)
+    print(jdata)
+    user=list(filter(lambda u: str(u['Id'])== Id, jdata))
+    print(user)
+    return jsonify(user)
 
 @app.route('/image', methods = ['GET'])
 def wait_for_image_result():
@@ -45,14 +58,9 @@ def result():
     user_final = "Sargam"
     df = pd.read_excel("test.xlsx")
 
-    selected_user = df[df["Name"] == user_final]
-    dataf = pd.DataFrame({'Name': selected_user["Name"]})
-
-
-
+    selected_user = df[df["Name"] == image_result]
+    dataf = pd.DataFrame({'Name': selected_user["Name"]}) 
     return render_template('index.html',result=image_result, tables=[dataf.to_html()])
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
