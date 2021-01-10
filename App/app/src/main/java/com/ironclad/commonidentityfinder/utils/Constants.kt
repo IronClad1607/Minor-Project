4c25f8e19c3d01b5fec8321abd07309920985520
Ishaan Jain <ishaan160799@gmail.com>
2021-01-11 00:18:33 +0530

    login/sign up done

--- App/app/src/main/java/com/ironclad/commonidentityfinder/utils/Constants.kt
+++ App/app/src/main/java/com/ironclad/commonidentityfinder/utils/Constants.kt
@@ -4,6 +4,7 @@ class Constants {
 
     companion object {
         const val SPLASH_DELAY: Long = 3000
+        const val LOGIN_DELAY: Long = 1000
         const val SCREEN_TAG = "screen tag"
     }
 }
